3c68623775469f5c1f0a55026eddf7fe11a498c2
Maha <mahafarghalymo@gmail.com>
2023-12-26 14:31:13 -0800

    Add for loop to print java for 10 n

--- hello_iti.java
+++ hello_iti.java
@@ -6,6 +6,9 @@ public class HelloITI {
     public static void main(String[] args) {
         System.out.println("Hello ITI");
 		    System.out.println("Hello Git");
+			for(int i =0;i<10;i++){
+				system.out.println("Java");
+			}
     }
     
 }
