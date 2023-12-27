e00e95f18b5aba4250725e2b8a810f3ef598c9be
Aya Mashaly <ayamashaly@gmail.com>
2023-12-27 14:43:56 +0200

    add two line

--- hello_iti.java
+++ hello_iti.java
@@ -5,8 +5,11 @@ public class HelloITI {
      */
     public static void main(String[] args) {
 String b,c;
-        System.out.println("Hello ITI");
-		    System.out.println("Hello Git");
+		    System.out.println("Hello Git");    
+                System.out.println("Hello ITI");
+System.out.println("Hello Git");    
+                System.out.println("Hello ITI");
+
 			/*for(int i =0;i<10;i++){
 				system.out.println("Java");
 					system.out.println(" ");
